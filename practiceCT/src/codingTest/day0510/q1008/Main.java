698e0b86f1074bcf7c6cb34d362b2271d41818fe
[Miso0320] <[miso8493@hanmail.net]>
2023-05-23 19:24:45 +0900

    백준 코딩테스트 연습 코드 전체 커밋

---
+++ practiceCT/src/codingTest/day0510/q1008/Main.java
@@ -0,0 +1,14 @@
+package codingTest.day0510.q1008;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		double A = sc.nextDouble();
+		double B = sc.nextDouble();
+		
+		System.out.println((int)A/B);
+	}
+}
---
+++ practiceCT/src/codingTest/day0511/q10869/Main.java
@@ -0,0 +1,7 @@
+package codingTest.day0511.q10869;
+
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+}
